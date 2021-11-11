9a7bf638f88416fdc6529f664adee2d42f8c5cdf
Leo Yu <leoyumail2000@gmail.com>
2021-11-10 17:04:01 -0800

    yes

--- tests/part-b/custom/inputs/save-instruction.s
+++ tests/part-b/custom/inputs/save-instruction.s
@@ -1,7 +1,10 @@
-addi x3 x0 15
-addi sp sp -4
-sw x3 0(sp)
-lw x4 0(sp)
-sb x3 0(sp)
-lb x4 0(sp)
-addi sp sp 4
+addi t1 x0 1025
+addi sp x0 1500
+sb t1 0(sp)
+lb s1 0(sp)
+sb t1 1(sp)
+lb s1 1(sp)
+sb t1 2(sp)
+lb s1 2(sp)
+sb t1 3(sp) 
+lb x4 3(sp)
